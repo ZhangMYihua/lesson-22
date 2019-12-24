86e6cf9d354671d26638967d1da517fb0089e082
Samir Sakanovic <SagiSan@users.noreply.github.com>
2019-12-24 22:56:14 +0100

    export fix
    Persistor should be exported, not persistStore. But the whole line 'export default {store, persistor} ' is uneccessary due to the above already exported store and persistor (even though its mentioned in video that they are not needed). But if you remove them, the 'export default' doesnt work. According to react it is better to use export default only if there is single export. (in this case we have 2 -> store and persistor). It should be exported like this, without export default

--- src/redux/store.js
+++ src/redux/store.js
@@ -9,5 +9,3 @@ const middlewares = [logger];
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
 export const persistor = persistStore(store);
-
-export default { store, persistStore };
